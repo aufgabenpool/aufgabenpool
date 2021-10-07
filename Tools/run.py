8dc41eab89548037b365ab904d9ba1331b6d6d01
Andreas Schwenk <andi.schwenk@googlemail.com>
2021-10-07 09:42:26 +0200

    sync

--- Tools/run.py
+++ Tools/run.py
@@ -13,6 +13,8 @@ from shutil import which
 
 print('Moodle Question Extractor - Author: Andreas Schwenk / TH Köln')
 
+skip_screenshots = '-noscreenshots' in sys.argv
+
 # 0.) Delete "../Data-tmp/"
 os.system("rm -rf ../Data-tmp/")
 os.system("mkdir -p ../Data-tmp/")
@@ -32,10 +34,11 @@ if x != 0:
     sys.exit(-1)
 
 # 3.) create screenshots as preview
-x = os.system('node get_preview_img_batch.js')
-if x != 0:
-    print('ERROR: failed to get preview images')
-    sys.exit(-1)
+if not skip_screenshots:
+    x = os.system('node get_preview_img_batch.js')
+    if x != 0:
+        print('ERROR: failed to get preview images')
+        sys.exit(-1)
 
 # 4.) postprocess screenshots (set background color transparent)
 if which('mogrify') is None:
