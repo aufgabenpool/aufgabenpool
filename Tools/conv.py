88d17a7c92dff7556575a487dd9e114f61cbaf9b
Andreas Schwenk <andi.schwenk@googlemail.com>
2021-10-14 10:39:28 +0200

    sync

--- Tools/conv.py
+++ Tools/conv.py
@@ -46,6 +46,8 @@ questionid = ''
 
 questionIdx = 0
 
+metadata["topic_hierarchy"] = {"":""}
+
 for i, question in enumerate(quiz):
     if question.tag is etree.Comment:
         questionid = question.text
@@ -59,25 +61,46 @@ for i, question in enumerate(quiz):
     questionType = question.attrib['type']
     if questionType == 'stack' and ('<type>checkbox</type>' in questionStr):
         questionType = 'stack-multichoice'
-    print(questionType)
+    #print(questionType)
     name = question.find('name')[0].text
     tested = False
     tags = question.find('tags')
-    taglist = []
+    q_tagset = {""}
     if tags is not None:
         for tag in tags:
             tag_name = tag[0].text
             if "getestet" in tag_name:
                 tested = True
-            taglist.append(tag_name)
-            tagset.add(tag_name)
+            q_tagset.add(tag_name.replace(":","_").lower())
+            tagset.add(tag_name.replace(":","_").lower())
+    q_tagset.remove("")
+
+    te1 = ""
+    te2 = ""
+    te3 = ""
+    for tag in tagset:
+        if tag.startswith("te_1_"):
+            te1 = tag
+        elif tag.startswith("te_2_"):
+            te2 = tag
+        elif tag.startswith("te_3_"):
+            te3 = tag
+            #metadata["topic_hierarchy"][tag] = {"":""}
+    
+    xxx
+    if len(te1) > 0 and te1 not in metadata["topic_hierarchy"]:
+        metadata["topic_hierarchy"][te1] = {"":""}
+        if len(te2) > 0 and te2 not in metadata["topic_hierarchy"][te1]:
+            metadata["topic_hierarchy"][te1][te2] = {"":""}
+
+
     #if not tested:
     #    continue
     metadata['exercises'].append({
         'importid': questionid,
         'id': questionIdx,
         'title': name,
-        'tags': taglist,
+        'tags': list(q_tagset),
         'category': current_category,
         'type': questionType}
     )
@@ -92,32 +115,33 @@ metadata["tags-all"] = list(tagset)
 
 
 
-with open("../Taxonomie/taxonomie.json") as f:
-    tax = json.load(f)
-    # TODO: crawl automatically from tax[..]
-    metadata["tags-maintopics"] = []
-    metadata["tags-didactics"] = tax["didactics"]
-    metadata["tags-content"] = tax["content"]
-    metadata["tags-difficulty"] = tax["difficulty"]
-    metadata["tags-status"] = tax["status"]
-    for maintopic in tax["maintopics"]:
-        metadata["tags-maintopics"].append(maintopic)
-        maintopic_tags = []
-        for ex in metadata['exercises']:
-            for tag in ex['tags']:
-                if tag == maintopic:
-                    continue
-                if tag in tax['didactics']:  # TODO: crawl automatically from tax[..]
-                    continue
-                if tag in tax['content']:
-                    continue
-                if tag in tax['difficulty']:
-                    continue
-                if tag in tax['ignore']:
-                    continue
-                if tag not in maintopic_tags:
-                    maintopic_tags.append(tag)
-        metadata["tags-maintopic-" + maintopic] = maintopic_tags
+# with open("../Taxonomie/taxonomie.json") as f:
+#     tax = json.load(f)
+#     # TODO: crawl automatically from tax[..]
+#     metadata["tags-maintopics"] = []
+#     #metadata["tags-didactics"] = tax["didactics"]
+#     #metadata["tags-content"] = tax["content"]
+#     #metadata["tags-difficulty"] = tax["difficulty"]
+#     #metadata["tags-status"] = tax["status"]
+#     for maintopic in tax["maintopics"]:
+#         metadata["tags-maintopics"].append(maintopic.lower())
+#         maintopic_tags = []
+#         for ex in metadata['exercises']:
+#             for tag in ex['tags']:
+#                 if tag == maintopic:
+#                     continue
+#                 if tag in tax['didactics']:  # TODO: crawl automatically from tax[..]
+#                     continue
+#                 if tag in tax['content']:
+#                     continue
+#                 if tag in tax['difficulty']:
+#                     continue
+#                 if tag in tax['ignore']:
+#                     continue
+#                 if tag not in maintopic_tags:
+#                     maintopic_tags.append(tag.lower())
+#         metadata["tags-maintopic-" + maintopic.lower()] = maintopic_tags
+
 
 
 
