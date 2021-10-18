1409c86901009793d8179b530a065c8cbdb1ac8a
Andreas Schwenk <andi.schwenk@googlemail.com>
2021-10-18 18:22:43 +0200

    sync

--- Tools/get_preview_img_batch.js
+++ Tools/get_preview_img_batch.js
@@ -2,12 +2,16 @@
 //  Moodle Question Preview
 //  (c) 2021 by TH Köln
 //  Author: Andreas Schwenk, andreas.schwenk@th-koeln.de
-//  Version: 0.01
+//  Version: 0.02
 //******************************************************************************
 
 // Note: This script is SLOW, since we need to wait for Maxima and MathJax...
+// In case that "old_content_path" already contains screenshots and moodle-xml
+// files in paths "img_out_path" and "old_content_path", we can simply copy
+// old screenshots.
 
 const fs = require("fs");
+const { exec } = require("child_process");
 const puppeteer = require('puppeteer');
 
 // preferences
@@ -17,84 +21,97 @@ const moodle_pwd = 'dGDs988S#';  // TODO: must be secret!!!!!
 const course_id = 2; // TODO: this is static...
 const meta_data_path = "../Data-tmp/meta.json";
 const img_out_path = '../Data-tmp/';
-
+const old_content_path = '../Data/';
 let first_exercise_id = 0;
-let last_exercise_id = 1e12;
+let last_exercise_id = 1e12; // only used for debugging purposes
 
 if(process.argv.length == 4) {
     first_exercise_id = parseInt(process.argv[2]);
     last_exercise_id = parseInt(process.argv[3]);
 }
 
-// exercise meta data
+// get exercise meta data
 let meta = fs.readFileSync(meta_data_path);
 meta = JSON.parse(meta);
 let exercises = meta["exercises"];
 
 // take screenshots
 (async() => {
-
     // init puppeteer
     const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
-
     // goto login page
     const page = await browser.newPage();
     await page.goto(moodle_url + '/login/index.php', {waitUntil: 'load'});
-
     // login with user data
     const userInput = await page.waitForSelector("#username");
     await userInput.focus();
     await userInput.type(moodle_user);
-
     const userPassword = await page.waitForSelector("#password");
     await userPassword.focus();
     await userPassword.type(moodle_pwd);
-
     await page.click('#loginbtn');
     await page.waitForNavigation(); 
-
     // for all exercises...
     for(let i=first_exercise_id; i<exercises.length && i<last_exercise_id; i++) {
+        process.stdout.write(".");
         let exercise = exercises[i];
         let question_id = exercise["id"];
-
-        for(let k=0; k<3; k++) { // create 3 scrrenshot to show random behavior
-            let screenshot_path = img_out_path + exercise["id"] + "_" + k + ".png";
-
-            try {
-                // goto question preview
-                await page.goto(
-                    moodle_url 
-                    + '/question/preview.php?id=' 
-                    + question_id 
-                    + '&courseid=' 
-                    + course_id, 
-                    {waitUntil: 'load'});
-
-                // wait for MathJax rendering
-                await new Promise(resolve => setTimeout(resolve, 1500));
-
-                // take screenshot
-                const questionDiv = await page.$(".content");
-                const box = await questionDiv.boundingBox();
-                const yoffset = 12;  // clip links at top-right position
-                await questionDiv.screenshot({
-                    'path': screenshot_path,
-                    'clip': {'x': box['x'], 'y': box['y']+yoffset, 
-                            'width': box['width'], 'height': box['height']-yoffset}
-                });
-            } catch(e) {
-                console.log("failed to get screenshot for exercise " + i);
-            }
-        
+        // only get screenshots in case the XML file updates since last sync
+        let must_update = true;
+        if(fs.existsSync(old_content_path + question_id + ".xml") 
+                && fs.existsSync(old_content_path + question_id + "_0.png")
+                && fs.existsSync(old_content_path + question_id + "_1.png")
+                && fs.existsSync(old_content_path + question_id + "_2.png")) {
+            let xml_old = fs.readFileSync(old_content_path + question_id + ".xml", 'utf8');
+            let xml_new = fs.readFileSync(img_out_path + question_id + ".xml", 'utf8');
+            //console.log(xml_old)
+            //console.log(xml_new)
+            must_update = xml_old !== xml_new;
+            //console.log(must_update)
         }
-
+        if(must_update == false) {
+            // copy existing image files
+            for(let k=0; k<3; k++) {
+                fs.copyFileSync(old_content_path + question_id + "_" + k + ".png", 
+                                img_out_path + question_id + "_" + k + ".png");
+            }
+        } else {
+            // create 3 separate screenshots (screenshots should be different 
+            // for questions that include random variables)
+            for(let k=0; k<3; k++) {
+                let screenshot_path = img_out_path + question_id + "_" + k + ".png";
+                try {
+                    // goto question preview
+                    await page.goto(
+                        moodle_url 
+                        + '/question/preview.php?id=' 
+                        + question_id 
+                        + '&courseid=' 
+                        + course_id, 
+                        {waitUntil: 'load'});
+                    // wait for MathJax rendering
+                    await new Promise(resolve => setTimeout(resolve, 1500));
+                    // take screenshot
+                    const questionDiv = await page.$(".content");
+                    const box = await questionDiv.boundingBox();
+                    const yoffset = 12;  // clip links at top-right position
+                    await questionDiv.screenshot({
+                        'path': screenshot_path,
+                        'clip': {'x': box['x'], 'y': box['y']+yoffset, 
+                                'width': box['width'], 'height': box['height']-yoffset}
+                    });
+                } catch(e) {
+                    console.log("failed to get screenshot for exercise " + i);
+                }
+                // postprocess screenshot (set background color transparent)
+                // TODO: what happens if 'mogrify' is NOT installed???
+                exec('cd ../Data-tmp/ && mogrify -format png -fill "#FFFFFF" -opaque "#E7F3F5" ' 
+                    + question_id + "_" + k + ".png");
+            }
+        }
     }
-
     // destroy puppeteer
     browser.close();
-
     // bye..
     console.log("..ready")
-
 })();
--- Tools/run.py
+++ Tools/run.py
@@ -16,10 +16,12 @@ print('Moodle Question Extractor - Author: Andreas Schwenk / TH Köln')
 skip_screenshots = '-noscreenshots' in sys.argv
 
 # 0.) Delete "../Data-tmp/"
+print("removing directory '../Data-tmp/'")
 os.system("rm -rf ../Data-tmp/")
 os.system("mkdir -p ../Data-tmp/")
 
 # 1.) Download question pool as moodle-xml file
+print("downloading question pool from moodle")
 x = os.system('node download_pool.js')
 if x != 0:
     print('ERROR: failed to get question pool as moodle-xml file.')
@@ -28,12 +30,14 @@ moodle_xml_files = glob.glob("../Data-tmp/*.xml")
 moodle_xml_path = max(moodle_xml_files, key=os.path.getctime)  # get newest file
 
 # 2.) create meta.json + an xml-file for each question
+print("extracting metadata from moodle-xml")
 x = os.system('python3 conv.py ' + moodle_xml_path + ' ../Data-tmp/')
 if x != 0:
     print('ERROR: failed to convert question pool')
     sys.exit(-1)
 
 # 3.) create screenshots as preview
+print("creating screenshots")
 if not skip_screenshots:
     x = os.system('node get_preview_img_batch.js')
     if x != 0:
@@ -41,13 +45,15 @@ if not skip_screenshots:
         sys.exit(-1)
 
 # 4.) postprocess screenshots (set background color transparent)
-if not skip_screenshots:
-    if which('mogrify') is None:
-        print("warning: imagemagick is not installed!")
-    else:
-        os.system('cd ../Data-tmp/ && mogrify -format png -fill "#FFFFFF" -opaque "#E7F3F5" *.png')
+#print("postprocessing screenshots")
+#if not skip_screenshots:
+#    if which('mogrify') is None:
+#        print("warning: imagemagick is not installed!")
+#    else:
+#        os.system('cd ../Data-tmp/ && mogrify -format png -fill "#FFFFFF" -opaque "#E7F3F5" *.png')
 
 # 5.) Replace current "../Data/" directory. This is done only in case no error occourred. Otherwise, the old version remains
+print("moving directoy '../Data-tmp' to '../Data'")
 os.system("rm -rf ../Data/")
 os.system("mv ../Data-tmp ../Data")
 
