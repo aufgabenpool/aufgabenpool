29763f8b6827f27f6ad744c5cdff124c46acb679
Andreas Schwenk <andi.schwenk@googlemail.com>
2021-12-27 11:22:17 +0100

    sync

--- index.php
+++ index.php
@@ -1,74 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'head.php'; ?>
-<body>
+    <?php include 'head.php'; ?>
+    <body>
 
-    <?php include 'title.php'; ?>
+        <?php include 'title.php'; ?>
 
-    <br/>
+        <br/>
 
-    <div class="container px-0">
-        <div class="row px-0">
-            <div class="col-lg py-1">
-                <div class="card h-100 border-0">
-                    <img class="card-img-top my-2" src="img/sheets-2.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Verwendbar für</h5>
-                        <p class="card-text">
-                            <ul>
-                                <li>Hausaufgaben</li>
-                                <li>Übung und Training</li>
-                                <li>Prüfungsvorleistungen</li>
-                                <li>Digitale Prüfungen</li>
-                            </ul>
-                        </p>
+        <div class="container px-0">
+            <div class="row px-0">
+                <div class="col-lg py-1">
+                    <div class="card h-100 border-0">
+                        <img class="card-img-top my-2" src="img/sheets-2.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Verwendbar für</h5>
+                            <p class="card-text">
+                                <ul>
+                                    <li>Hausaufgaben</li>
+                                    <li>Übung und Training</li>
+                                    <li>Prüfungsvorleistungen</li>
+                                    <li>Digitale Prüfungen</li>
+                                </ul>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg py-1">
-                <div class="card h-100 border-0">
-                    <img class="card-img-top my-2" src="img/tax-2.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Eingesetzte Taxonomien</h5>
-                        <p class="card-text">
-                            <ul>
-                                <li>Bloom et al. (1972)</li>
-                                <li>Maier u.a. (2014)</li>
-                                <li>Aufgabentypen</li>
-                                <li>Inhaltsbereiche</li>
-                            </ul>
-                        </p>
+                <div class="col-lg py-1">
+                    <div class="card h-100 border-0">
+                        <img class="card-img-top my-2" src="img/tax-2.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Eingesetzte Taxonomien</h5>
+                            <p class="card-text">
+                                <ul>
+                                    <li>Bloom et al. (1972)</li>
+                                    <li>Maier u.a. (2014)</li>
+                                    <li>Aufgabentypen</li>
+                                    <li>Inhaltsbereiche</li>
+                                </ul>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg py-1">
-                <div class="card h-100 border-0">
-                    <img class="card-img-top my-2" src="img/icon-new-2.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Aufgabenpool</h5>
-                        <p class="card-text">Zentrale Sammlung von STACK-basierten Mathematikaufgaben für Moodle und Ilias.</p>
-                        <p class=""><a href="pool.php" class="btn btn-primary btn-danger">Link</a></p>
+                <div class="col-lg py-1">
+                    <div class="card h-100 border-0">
+                        <img class="card-img-top my-2" src="img/icon-new-2.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Aufgabenpool</h5>
+                            <p class="card-text">Zentrale Sammlung von STACK-basierten Mathematikaufgaben für Moodle und Ilias.</p>
+                            <p class=""><a href="pool.php" class="btn btn-primary btn-danger">Link</a></p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg py-1">
-                <div class="card h-100 border-0">
-                    <img class="card-img-top my-2" src="img/moodle.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Fragendatenbank</h5>
-                        <p class="card-text">Moodle-Kurs zur Verwaltung der Fragendatenbank (mit beschränktem Zugriff).</p>
-                        <p class=""><a href="https://aufgabenpool.f07-its.fh-koeln.de/moodle/login/index.php" class="btn btn-primary btn-danger" target="_blank">Link</a></p>
+                <div class="col-lg py-1">
+                    <div class="card h-100 border-0">
+                        <img class="card-img-top my-2" src="img/moodle.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Fragendatenbank</h5>
+                            <p class="card-text">Moodle-Kurs zur Verwaltung der Fragendatenbank (mit beschränktem Zugriff).</p>
+                            <p class=""><a href="https://aufgabenpool.f07-its.fh-koeln.de/moodle/login/index.php" class="btn btn-primary btn-danger" target="_blank">Link</a></p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <script>
-        document.getElementById("link-start").innerHTML = '<b><span class="text-danger">Startseite</span></b>';
-    </script>
+        <script>
+            document.getElementById("link-start").innerHTML = '<b><span class="text-danger">Startseite</span></b>';
+        </script>
 
-    <?php include 'footer.php'; ?>
+        <?php include 'footer.php'; ?>
 
-    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+
+</html>
--- pool.php
+++ pool.php
@@ -1,69 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'head.php'; ?>
-<body>
-    <?php include 'title.php'; ?>
-
-    <br/>
-
-    <div class="container col bg-white">
+    <?php include 'head.php'; ?>
+    <body>
+        <?php include 'title.php'; ?>
 
         <br/>
-        
-        <div class="row py-2">
-            <div class="col">
-            </div>
-            <div class="col text-center">
-                <div class="btn-group">
-                    <a id="pool-button" onclick="aufgabenpool.clicked_on_pool_tab();" class="btn btn-danger active" aria-current="page">Fragensammlung</a>
-                    <a id="basket-button" onclick="aufgabenpool.clicked_on_basket_tab();" class="btn btn-outline-danger">Aufgabenblatt</a>
+
+        <div class="container col bg-white">
+
+            <br/>
+            
+            <div class="row py-2">
+                <div class="col">
                 </div>
-                
-            </div>
-            <div class="col text-end">
-                <span class="nav-link disabled">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stand: <span id="pool-date"></span></span>
-            </div>
-        </div>
-
-        <!--<div class="row px-3">
-            <div class="col">
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Suche" aria-label="Search">
-                    < !--<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>-- >
-                </form>
-            </div>
-        </div>-->
-
-        <div class="row">
-            <div class="col bg-white">
-                <div>
-                    <div id="taglist_div" class="m-3"></div>
-                    <!--<div id="exercises_div" class="m-3"></div>
-                    <div id="basket_div" class="m-3" style="display: none;"></div>-->
+                <div class="col text-center">
+                    <div class="btn-group">
+                        <a id="pool-button" onclick="aufgabenpool.clicked_on_pool_tab();" class="btn btn-danger active" aria-current="page">Fragensammlung</a>
+                        <a id="basket-button" onclick="aufgabenpool.clicked_on_basket_tab();" class="btn btn-outline-danger">Aufgabenblatt</a>
+                    </div>
+                    
                 </div>
-            </div>    
+                <div class="col text-end">
+                    <span class="nav-link disabled">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stand: <span id="pool-date"></span></span>
+                </div>
+            </div>
+
+            <!--<div class="row px-3">
+                <div class="col">
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Suche" aria-label="Search">
+                        < !--<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>-- >
+                    </form>
+                </div>
+            </div>-->
+
+            <div class="row">
+                <div class="col bg-white">
+                    <div>
+                        <div id="taglist_div" class="m-3"></div>
+                        <!--<div id="exercises_div" class="m-3"></div>
+                        <div id="basket_div" class="m-3" style="display: none;"></div>-->
+                    </div>
+                </div>    
+            </div>
         </div>
-    </div>
 
-    <div class="container">
-        <div class="row mx-0">
-            <div class="col mx-0">
-                <!--<div class="mx-0">-->
-                    <!--<div id="taglist_div" class="m-3"></div>-->
-                    <div id="exercises_div" class="mx-0 my-3"></div>
-                    <div id="basket_div" class="mx-0 my-3" style="display: none;"></div>
-                <!--</div>-->
-            </div>    
+        <div class="container">
+            <div class="row mx-0">
+                <div class="col mx-0">
+                    <!--<div class="mx-0">-->
+                        <!--<div id="taglist_div" class="m-3"></div>-->
+                        <div id="exercises_div" class="mx-0 my-3"></div>
+                        <div id="basket_div" class="mx-0 my-3" style="display: none;"></div>
+                    <!--</div>-->
+                </div>    
+            </div>
         </div>
-    </div>
 
-    <script>
-        document.getElementById("link-pool").innerHTML = '<b><span class="text-danger">Aufgabenpool</span></b>';
-    </script>
+        <script>
+            document.getElementById("link-pool").innerHTML = '<b><span class="text-danger">Aufgabenpool</span></b>';
+        </script>
 
-    <?php include 'footer.php'; ?>
+        <?php include 'footer.php'; ?>
 
-    <script src="build/js/aufgabenpool.min.js?v=<?php echo time();?>"></script>
+        <script src="build/js/aufgabenpool.min.js?v=<?php echo time();?>"></script>
 
-    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
