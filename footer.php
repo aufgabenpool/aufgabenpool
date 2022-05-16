22b9efcf7711e817d9096bce398e08786837e680
Andreas Schwenk <andi.schwenk@googlemail.com>
2022-05-16 12:51:09 +0200

    changed license info

--- footer.php
+++ footer.php
@@ -5,18 +5,30 @@
     <div class="row">
         <div class="col pb-3">
 
-            <p class="text-center" style="font-size: 36px;">
-                <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.de" target="_blank" class="link-light text-decoration-none">
-                    <i class="fab fa-creative-commons"></i>
-                    <i class="fab fa-creative-commons-by"></i>
-                    <i class="fab fa-creative-commons-sa"></i>
-                    &nbsp;(CC BY-SA 4.0 DE)
-                </a>
-            </p>
+
+        <p class="text-center" style="font-size: 36px;">
+            <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.de" target="_blank" class="link-light text-decoration-none">
+                <i class="fab fa-creative-commons"></i>
+                <i class="fab fa-creative-commons-by"></i>
+                <i class="fab fa-creative-commons-sa"></i>
+                &nbsp;(CC BY-SA 4.0 DE)
+            </a>
+        </p>
+
+        <p>
+        "Digitaler Aufgabenpool Mathematik – Kompetenzorientiertes digitales Prüfen" von
+<a href="https://www.th-koeln.de/personen/heiko.knospe/" class="link-light text-decoration-none">Prof. Dr. Heiko Knospe</a>,
+<a href="https://www.th-koeln.de/personen/jan-philipp.schmidt/" class="link-light text-decoration-none">Prof. Dr. Jan-Philipp Schmidt</a>,
+<a href="https://www.th-koeln.de/personen/angela.schmitz/" class="link-light text-decoration-none">Prof. Dr. Angela Schmitz</a>,
+<a href="https://www.th-koeln.de/personen/patricia_maria.graf/" class="link-light text-decoration-none">Patricia Maria Graf, B.Sc.</a>,
+<a href="https://www.th-koeln.de/personen/jan.reissner/" class="link-light text-decoration-none">Jan Siegfried Reißner, M.Sc.</a> und
+<a href="https://www.th-koeln.de/personen/andreas.schwenk/" class="link-light text-decoration-none">Andreas Schwenk, M.Sc.</a>
+unter der Lizenz <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.de" class="link-light">CC BY-SA 4.0 DE</a> via <a href="https://aufgabenpool.th-koeln.de" class="link-light">aufgabenpool.th-koeln.de</a>.
+        </p>
 
             <small>&#169; <?php echo date("Y");?>
-                TH Köln
-                &bullet;
+                TH Köln.
+                <!--&bullet;
                 <a href="https://www.th-koeln.de/personen/heiko.knospe/" class="link-light text-decoration-none">Prof. Dr. Heiko Knospe</a>
                 &bullet;
                 <a href="https://www.th-koeln.de/personen/jan-philipp.schmidt/" class="link-light text-decoration-none">Prof. Dr. Jan-Philipp Schmidt</a>
@@ -27,10 +39,9 @@
                 &bullet;
                 <a href="https://www.th-koeln.de/personen/jan.reissner/" class="link-light text-decoration-none">Jan Siegfried Reißner, M.Sc.</a>
                 &bullet;
-                <a href="https://www.th-koeln.de/personen/andreas.schwenk/" class="link-light text-decoration-none">Andreas Schwenk, M.Sc.</a>
+                <a href="https://www.th-koeln.de/personen/andreas.schwenk/" class="link-light text-decoration-none">Andreas Schwenk, M.Sc.</a>-->
             </small>
             &nbsp;&nbsp;&nbsp;
-            <br/>
             <small>
                 Kontakt Webauftritt:&nbsp;&nbsp;<a class="link-light" href="https://www.th-koeln.de/personen/andreas.schwenk/" target="_blank">Andreas Schwenk</a>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#x2015;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
