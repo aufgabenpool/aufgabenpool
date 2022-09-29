65c34d05249ab6de09b0c8c4d5ae92645f5eb6b9
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-09-29 08:45:57 +0200

    Update help-user.php

--- help-user.php
+++ help-user.php
@@ -9,28 +9,30 @@
 
         <div class="container bg-white">
             <div class="row py-2">
-                <div class="col-lg text-start text-danger">
-                    Hinweis: Diese Hilfeseite befindet sich momentan noch im Aufbau
+                <div class="col text-start m-2 lead">
+                    <div id="userguide"></div>
                 </div>
             </div>
         </div>
-
-        <div id="userguide"></div>
 
         <script>
             function generateContent(id) {
                 axios.get("help/" + id + ".md").then(function (response) {
                     html = new markdownit().render(response.data);
-                    document.getElementById(id).innerHTML += `
-                    <br/>
-                    <div class="container bg-white">
-                        <div class="row py-2">
-                            <div class="col-lg text-start">
-                                ` + html + `
-                            </div>
-                        </div>
-                    </div>
-                    `;
+
+                    //html = html.replace(/<p>/g, "<p class=\"lead\">");
+                    html = html.replace(/\"images/g, "\"help/images");
+                    html = html.replace(/\<img src/g, "<img class=\"img-fluid shadow mx-5 my-2 p-2 w-75 rounded\" src");
+                    html = html.replace(/<\/h1>/g, "</h1><hr/>");
+                    html = html.replace(/<h2>/g, "<br/><h2>");
+                    html = html.replace(/<\/h2>/g, "</h2><hr/>");
+                    html = html.replace(
+                        '<p><img class="img-fluid shadow mx-5 my-2 p-2 w-75 rounded" src="help/images/moodle-ilias.png" alt=""></p>',
+                        '<p><img class="img-fluid shadow mx-5 my-2 p-2 w-33 rounded" src="help/images/moodle-ilias.png" alt=""></p>');
+
+                    console.log(html);
+
+                    document.getElementById(id).innerHTML = html;
                 });
             }
 
