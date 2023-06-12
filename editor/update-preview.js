a00737b0c2c86c93284297f1d5e550766f3f10eb
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-06-12 13:30:05 +0200

    Update update-preview.js

--- editor/update-preview.js
+++ editor/update-preview.js
@@ -94,7 +94,7 @@ connection.query(query, [], function (error, results, fields) {
                     { waitUntil: 'load', timeout: 0 },
                 );
                 // wait for MathJax rendering
-                await new Promise((resolve) => setTimeout(resolve, 1500));
+                await new Promise((resolve) => setTimeout(resolve, 2500)); // TODO: was 1500
 
                 // remove upper-right links (https://stackoverflow.com/questions/50867065/puppeteer-removing-elements-by-class)
                 let div_selector_to_remove = '.questiontestslink';
